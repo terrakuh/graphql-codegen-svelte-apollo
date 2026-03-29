24d578898a7ede997eb5f412980ce44a0114a8f9
terrakuh <16595452+terrakuh@users.noreply.github.com>
2026-03-29 12:33:08 +0200

    Fix build

--- src/fragments.ts
+++ src/fragments.ts
@@ -1,8 +1,7 @@
 import { Types } from "@graphql-codegen/plugin-helpers";
 import { ClientSideBaseVisitor, LoadedFragment } from "@graphql-codegen/visitor-plugin-common";
-import { concatAST, GraphQLSchema, Kind, OperationTypeNode, visit } from "graphql";
+import { concatAST, GraphQLSchema, Kind, visit } from "graphql";
 import { Config } from "./config";
-import { pascalCase } from "change-case";
 
 export function generateFragments(schema: GraphQLSchema, documents: Types.DocumentFile[], config: Config) {
 	const allFragments = documents
@@ -23,26 +22,5 @@ export function generateFragments(schema: GraphQLSchema, documents: Types.Docume
 	const documentNodes = documents.map((document) => document.document).filter((document) => document != null);
 	const result = visit(concatAST(documentNodes), visitor);
 
-	return result.definitions.join("\n\n");
-	// const allAst = concatAST(documents.map((d) => d.document));
-
-	// const allFragments: LoadedFragment[] = [
-	// 	...(allAst.definitions.filter((d) => d.kind === Kind.FRAGMENT_DEFINITION) as FragmentDefinitionNode[]).map(
-	// 		(fragmentDef) => ({
-	// 			node: fragmentDef,
-	// 			name: fragmentDef.name.value,
-	// 			onType: fragmentDef.typeCondition.name.value,
-	// 			isExternal: false,
-	// 		}),
-	// 	),
-	// 	...(config.externalFragments ?? []),
-	// ];
-
-	// const visitor = new ClientSideBaseVisitor(schema, allFragments, {}, { documentVariableSuffix: "Doc" }, documents);
-
-	// const visitorResult = visit(allAst, visitor);
-
-	// const operations = allAst.definitions.filter(
-	// 	(d) => d.kind === Kind.OPERATION_DEFINITION,
-	// ) as OperationDefinitionNode[];
+	return [visitor.fragments, ...result.definitions].join("\n\n");
 }
